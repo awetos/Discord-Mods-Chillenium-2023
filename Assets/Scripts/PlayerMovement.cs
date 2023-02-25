6fe9b5ef691f417f02fae562f9d188402b8b40d5
Devsignerz  <Devsignerz@gmail.com>
2023-02-24 22:04:51 -0600

    character movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,5 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
-    
+    public float speed;//player move speed
+	[SerializeField] Vector3 cameraOffset;//camera movement offset from player position
+	[SerializeField] float cameraSmoothness;//camera movement smoothness
+
+	void FixedUpdate() {
+
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, transform.position, cameraSmoothness)+cameraOffset;//smoothly follow player
+
+		float hor = Input.GetAxisRaw("Horizontal");//get left right movement input
+		float ver = Input.GetAxisRaw("Vertical");//get up down movement input
+
+		Vector3 mov = new Vector3(hor, 0, ver).normalized * speed;//set movement velocity and direction based on input
+		print(mov);
+		GetComponent<Rigidbody>().velocity = mov;//move player
+	}
 }
